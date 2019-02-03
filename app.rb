76069a7e1928b15ca77d0cf3b0c17cccfbd71cb0
Priscilla H <priscillamhuynh@gmail.com>
2019-02-03 13:26:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i 
     @phrase = params[:phrase]
     phrase = ""
-    @number.times do {phrase << @phrase + "\n"}
+    @number.times  {phrase << @phrase + "\n"}
     phrase
   end
 end
