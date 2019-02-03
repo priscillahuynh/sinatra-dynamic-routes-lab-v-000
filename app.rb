3a678cc83eeafa49068d96eed0a747248f6a54b7
Priscilla H <priscillamhuynh@gmail.com>
2019-02-03 13:20:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,11 +12,10 @@ class App < Sinatra::Base
     "#{@number * @number}"
   end
   
-  get 'say/:number/:phrase' do 
+  get '/say/:number/:phrase' do 
     @number = params[:number].to_i 
     @phrase = params[:phrase]
     phrase = ""
     @number.times {puts "#{@phrase}"}
-    
   end
 end
