f1cf8179f82140c4ea9e84b1cdb3ff61b429a5fa
Priscilla H <priscillamhuynh@gmail.com>
2019-02-03 13:34:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,19 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @operation = params[:operation]
+    case @operation
+    when 'add'
+      "#{@num1 + @num2}"
+    when 'multiply'
+      "#{@num1 * @num2}"
+    when 'subtract'
+      "#{@num1 - @num2}"
+    when 'divide'
+      "#{@num1 / @num2}"
+    end
   end
 end
 
