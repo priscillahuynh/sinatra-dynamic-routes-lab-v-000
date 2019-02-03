bd9547a7e9a51426128e88d292db2516d4aab576
Priscilla H <priscillamhuynh@gmail.com>
2019-02-03 11:57:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do 
-    @name = param[:name]
+    @name = params[:name]
     "#{@name}.reverse"
   end
 end
